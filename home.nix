b0a3c6d733692f5c619139de2f846446dc2aaf86
OmarHdz <omarhg_@hotmail.com>
2025-04-12 02:21:41 +0000

    tmux at start and git config

--- home.nix
+++ home.nix
@@ -81,6 +81,31 @@
       enable = true;
     };
 
+ programs.git = {
+    enable = true; # Asegúrate de que Git esté habilitado
+
+    # --- Configuraciones básicas ---
+    userName = "OmarHdz";
+    userEmail = "omarhg_@hotmail.com";
+
+    # --- Configuraciones adicionales (equivalente a git config --global key value) ---
+    # Usa 'extraConfig' para la mayoría de las opciones
+    extraConfig = {
+      # init.defaultBranch = "main"; # Cambia la rama por defecto a 'main'
+      # pull.rebase = true;          # Prefiere rebase en lugar de merge al hacer pull
+      # github.user = "tu_usuario_github"; # Útil para herramientas como 'hub' o 'gh'
+      # # ... otras opciones ...
+      #
+      # # Ejemplo de alias
+      # alias = {
+      #   st = "status -sb";
+      #   co = "checkout";
+      #   br = "branch";
+      #   ci = "commit";
+      #   ll = "log --pretty=format:'%C(yellow)%h %C(cyan)%>(12)%ar %C(magenta)%<(10)%aN %C(auto)%d %Creset%s'";
+       };
+      };
+
   # programs.bash.enable = true;
   programs.bash = {
     enable = true;
@@ -104,6 +129,7 @@
      };
      initExtra = ''
         eval "$(oh-my-posh init zsh --config '${config.xdg.configHome}/ohmyposh/mytheme.json')"
+        test -z "$TMUX" && tmux
      '';
   };
 
