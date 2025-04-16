f367947ca64b44aad1fef269533bdd3d4fcdd9ef
OmarHdz <omarhg_@hotmail.com>
2025-04-16 21:55:30 +0000

    Add template devenv.nix

---
+++ docs/devenv.nix
@@ -0,0 +1,41 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  languages.python = {
+      enable = true;
+      version = "3.11";
+      venv.enable = true;
+      uv.enable = true;
+      # venv.requirements = ''
+      #   requests
+      #   ipython
+      #   numpy
+      #   pandas
+      #   flask
+      # '';
+    };
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  enterShell = ''
+    hello
+    git --version
+    uv sync
+  '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+}
