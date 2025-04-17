c6141af6bd0e9d4248626b6f368bce9dd23ea29c
OmarHdz <omarhg_@hotmail.com>
2025-04-17 02:40:59 +0000

    Add special keys load command

--- modules/home-manager/shells/sh.nix
+++ modules/home-manager/shells/sh.nix
@@ -43,6 +43,7 @@ in
         eval "$(direnv hook zsh)"
         # Bind ctrl-r but not up arrow
         eval "$(atuin init zsh --disable-up-arrow)"
+        # source ~/.special_keys
      '';
 
     # --- Mueve la configuración de Oh My Zsh aquí ---
