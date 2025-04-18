30d799b4fa338691b0e5ff9aca0643e71b87d5f7
OmarHdz <omarhg_@hotmail.com>
2025-04-18 23:44:52 +0000

    Indicator nix shell in oh my posh

--- modules/home-manager/editors/nvim/lua/config/options.lua
+++ modules/home-manager/editors/nvim/lua/config/options.lua
@@ -8,7 +8,7 @@ vim.opt.clipboard = "unnamedplus"
 function RunPythonInTmux()
   vim.cmd("w")
   local file = vim.fn.expand("%")
-  local command = string.format("echo ' > python %s ';  python %s; zsh", file, file)
+  local command = string.format("sleep 2; echo ' > python %s ';  python %s; zsh", file, file)
   local tmux_cmd = string.format('tmux split-window -d -v -p 20 "%s"', command)
   vim.fn.system(tmux_cmd)
   vim.cmd("redraw!")
